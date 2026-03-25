1073fffa9de92e273e197b8a644a4b47ef2996f7
SebastianCA19 <castroarrietasebastiandavid@gmail.com>
2026-03-25 01:01:49 -0500

    fix: Update build.gradle.kts to use specific compileSdk and ndkVersion; add .vscode to .gitignore; comment out flutter section in pubspec.yaml

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,8 +7,8 @@ plugins {
 
 android {
     namespace = "com.example.mi_negocio_unimagdalena"
-    compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    compileSdk = 35
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
